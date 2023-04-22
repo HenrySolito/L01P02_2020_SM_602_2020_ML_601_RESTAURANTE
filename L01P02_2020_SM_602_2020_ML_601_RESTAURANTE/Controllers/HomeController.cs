ff6333df9a3a1f1e509bfe29b0dc8f192e7fac47
hasol <henry.solito@catolica.edu.sv>
2023-04-22 10:15:32 -0600

    Add project files.

---
+++ L01P02_2020_SM_602_2020_ML_601_RESTAURANTE/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using L01P02_2020_SM_602_2020_ML_601_RESTAURANTE.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace L01P02_2020_SM_602_2020_ML_601_RESTAURANTE.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ L01P02_2020_SM_602_2020_ML_601_RESTAURANTE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L01P02_2020_SM_602_2020_ML_601_RESTAURANTE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
