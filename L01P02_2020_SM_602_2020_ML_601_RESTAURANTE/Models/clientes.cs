78f5e0c69be88dba3edbd4b5ecb58c6d35733a79
hasol <henry.solito@catolica.edu.sv>
2023-04-22 11:07:03 -0600

    creacion de modelo clientes

--- L01P02_2020_SM_602_2020_ML_601_RESTAURANTE/Models/clientes.cs
+++ L01P02_2020_SM_602_2020_ML_601_RESTAURANTE/Models/clientes.cs
@@ -1,6 +1,11 @@
-﻿namespace L01P02_2020_SM_602_2020_ML_601_RESTAURANTE.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace L01P02_2020_SM_602_2020_ML_601_RESTAURANTE.Models
 {
     public class clientes
     {
+        [Key]
+        public int clienteId { get; set; }
+        public string nombreCliente { get; set; }
+        public string direccion { get; set; }
     }
 }
