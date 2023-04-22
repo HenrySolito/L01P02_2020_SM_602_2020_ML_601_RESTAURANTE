3fe259675c6c35cb75eb62f86a073eca1b5d7a36
hasol <henry.solito@catolica.edu.sv>
2023-04-22 11:05:56 -0600

    Creacion de Modelo clientes y motoristas

--- L01P02_2020_SM_602_2020_ML_601_RESTAURANTE/Models/motoristas.cs
+++ L01P02_2020_SM_602_2020_ML_601_RESTAURANTE/Models/motoristas.cs
@@ -1,9 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+
 namespace L01P02_2020_SM_602_2020_ML_601_RESTAURANTE.Models
 {
     public class motoristas
     {
-       
+        [Key]
+        public int motoristaId { get; set; }
+        public string nombreMotorista { get; set; }
 
     }
 }
