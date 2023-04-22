917ff166637b039d173d9c4a41ca84999d2a6e22
Guillermo.Mezquita <guillermo.mezquita@catolica.edu.sv>
2023-04-22 11:05:00 -0600

    Modelos platos y pedidos

--- L01P02_2020_SM_602_2020_ML_601_RESTAURANTE/Models/pedidos.cs
+++ L01P02_2020_SM_602_2020_ML_601_RESTAURANTE/Models/pedidos.cs
@@ -1,6 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
 namespace L01P02_2020_SM_602_2020_ML_601_RESTAURANTE.Models
 {
     public class pedidos 
+    {
+        [Key]
+
+        public int pedidold { get; set; }
+        public int motoristald { get; set;}
+        public int clienteld { get; set;}
+        public int platold { get; set;}
+        public int cantidad { get; set;}
+        public int precio { get; set;}
     }
+    
 }
